85e337d3acace6bac517cd7777111c46ca1b23f4
keigo10 <keigoiphone0325@gmail.com>
2020-01-09 05:01:35 +0000

    customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,38 @@
 Rails.application.routes.draw do
+#loocalhost3000 ルーティング
+  root to: 'items#top'
+
+#コントローラーごとのルーティング
+#items
+  get 'items', to: 'items#index'
+  get 'items/:id', to: 'items#show'
+  #root toで設定しているため必要ない
+  #get '', to: 'items#top'
+#customers
+  get 'customers', to: 'customers#show'
+  get 'customers/edit', to: 'customers#edit'
+  patch 'customers', to: 'customers#update'
+  put 'customers', to: 'customers#update'
+  get 'customers/withdraw', to: 'customers#withdraw'
+  delete 'customers', to: 'customers#destory'
+#histories
+  get 'histories/information', to: 'histories#information'
+  get 'histories/really', to: 'histories#really'
+  post 'histories', to: 'histories#create'
+  get 'histories/thanks', to: 'histories#thanks'
+  get 'histories', to: 'histories#index'
+  get 'histories/show', to: 'histories#information'
+#cart_contents
+  get 'cart_contents', to: 'cart_contents#index'
+#shipping_addresses
+  get 'shipping_addresses', to: 'shipping_addresses#index'
+  delete 'shipping_addresses/:id', to: 'shipping_addresses#destoroy'
+  post 'shipping_addresses', to: 'shipping_addresses#create'
+  get 'shipping_addresses/edit', to: 'shipping_addresses#edit'
+  patch 'shipping_addresses', to: 'shipping_addresses#update'
+  put 'shipping_addresses', to: 'shipping_addresses#update'
+
+
   devise_for :admins, controllers:{
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
