08f15632755dc61f8029db2bd121fd0d6667cc67
toirai <larsalex8@gmail.com>
2020-01-09 13:04:19 +0900

    gemを追加したファイル

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # 　みや　namespace :admin do
+  end
   devise_for :admins, controllers:{
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
@@ -9,5 +11,8 @@ Rails.application.routes.draw do
   passwords:     'customers/passwords',
   registrations: 'customers/registrations'
   }
+    get 'admin/items'=>"admin/items#index"
+    get 'admin/items/new'=>"admin/items#new"
+    get 'admin/items/edit'=>"admin/items#edit"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
