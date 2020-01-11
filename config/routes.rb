ced62f29ea6e56592ae52b060038f5bc353adcfe
komiyama1006 <sim_white151@yahoo.co.jp>
2020-01-11 11:15:32 +0000

    2020/1/11

--- config/routes.rb
+++ config/routes.rb
@@ -50,16 +50,8 @@ devise_for :admins, controllers:{
   #put 'shipping_addresses', to: 'shipping_addresses#update'
     namespace :admin do
     resources :items
+    resources :histories
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get 'admin/items/top' => 'admin/items#top'
-
-  get 'admin/customers' => 'admin/customers#index'
-  get 'admin/customers/:id' => 'admin/customers#show'
-  get 'admin/customers/:id/edit' => 'admin/customers#edit'
-
-  get 'admin/histories' => 'admin/histories#index'
-  get 'admin/histories/show' => 'admin/histories#show'
 
 end
