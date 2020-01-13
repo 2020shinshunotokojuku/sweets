83366c3b052d0404114daf2a979eee5fcd091d65
toirai <larsalex8@gmail.com>
2020-01-13 20:32:24 +0900

    itemファイル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-devise_for :admins, controllers:{
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
+devise_for :admin, controllers:{
+  sessions:      'admin/sessions',
+  passwords:     'admin/passwords',
+  registrations: 'admin/registrations'
   }
 
   get '/customers_edit' => 'customers#edit', as: :edit_customers
