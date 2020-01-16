ead8d9e731c2be0bcdfda906934dd5acd27ed667
watanabeyoshinobu <nobu.regulus4946@gmail.com>
2020-01-16 10:11:53 +0000

    注文履歴一覧

--- app/controllers/admin/histories_controller.rb
+++ app/controllers/admin/histories_controller.rb
@@ -1,7 +1,14 @@
 class Admin::HistoriesController < ApplicationController
 
 	def index
+		if request.url.include?("admin/customers/:id")
+		 @histories = Customer.histories.all
+		 elsif request.referer&.include?("admin/top")
+		range = Date.yesterday.begining_of_day..Date.yesterday.end_of_day
+		@histories = History.where(created_at. range)
+		 else
 		@histories = History.page(params[:page]).reverse_order
+		 end
 	end
 
 	def show
