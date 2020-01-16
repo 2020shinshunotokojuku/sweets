36dda5bc722cd1507cd0d1fdacf422b72e5b662e
watanabeyoshinobu <nobu.regulus4946@gmail.com>
2020-01-16 06:31:46 +0000

    注文履歴詳細

--- app/controllers/admin/histories_controller.rb
+++ app/controllers/admin/histories_controller.rb
@@ -5,7 +5,11 @@ class Admin::HistoriesController < ApplicationController
 	end
 
 	def show
+		# ある１件の履歴データを取ってきてる
 		@history = History.find(params[:id])
+		# 全件の履歴データを取得
+		@histories = History.all
 	end
 
 end
+
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,4 +1,6 @@
 class History < ApplicationRecord
 	belongs_to :customer
 	has_many :history_details, dependent: :destroy
+
+enum order_status: [['nyuukinnmati', 0], ['nyuukinnmati', 1], ['zyunbityuu', 2],['hassouzumi', 3]]
 end
